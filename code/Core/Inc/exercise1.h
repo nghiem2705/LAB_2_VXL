9e275cbf2965ee7b684fc34ea24850ccfd80c471
nghiem2705 <nghiemgithub@gmail.com>
2025-10-05 19:23:56 +0700

    ex5

--- code/Core/Inc/exercise1.h
+++ code/Core/Inc/exercise1.h
@@ -22,4 +22,9 @@ void machine_state();
 //exercise3
 void update7SEG(int index);
 
+//exercise5
+extern int hour;
+extern int minute;
+extern int second;
+void updateClockBuffer();
 #endif /* INC_EXERCISE1_H_ */
